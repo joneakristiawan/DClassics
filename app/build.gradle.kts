e5159eb64c4d7babbeed6278d97411566069df0a
KevinKuslin <tyrantnitrox@gmail.com>
2026-06-30 09:44:00 +0700

    Downgraded AGP 8.13 to 8.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.dclassicsbooks"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.dclassicsbooks"
